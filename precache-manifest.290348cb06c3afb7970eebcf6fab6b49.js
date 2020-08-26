5cb71e3cc29b6b9e708e8bd304ac021a804cf892
Daniel Litvak <DanielLitvak5@gmail.com>
2020-08-26 18:18:17 +0300

    Updates

---
+++ precache-manifest.290348cb06c3afb7970eebcf6fab6b49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4821245490d0565d38ab1f4c4c6c84a",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "eeab8a291f1b3d8f369f",
+    "url": "/movie-search/static/css/main.fc66ecc5.chunk.css"
+  },
+  {
+    "revision": "239c5845fcd853daf73d",
+    "url": "/movie-search/static/js/2.0c09a7d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.0c09a7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeab8a291f1b3d8f369f",
+    "url": "/movie-search/static/js/main.eee7e2ec.chunk.js"
+  },
+  {
+    "revision": "8f1314dfdc21c95c9344",
+    "url": "/movie-search/static/js/runtime-main.8345f5b5.js"
+  }
+]);
